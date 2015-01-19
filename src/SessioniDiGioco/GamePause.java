3b4c13c8973da5c809031bfd99af75d673ab5190
giovanniromio <giovanniromio@localhost>
2015-01-19 12:07:39 +0100

    Aggiunta la pausa durante il gioco

---
+++ src/SessioniDiGioco/GamePause.java
@@ -0,0 +1,60 @@
+package SessioniDiGioco;
+
+import java.awt.Graphics2D;
+import java.awt.event.KeyEvent;
+
+public class GamePause extends SessioneDiGioco{
+	private String[] pausa={
+			"PAUSE",
+			"QUIT"
+	};
+	public GamePause(ControllerDiSessione cds){
+		this.cds = cds;
+	}
+	@Override
+	public void init() {
+		// TODO Auto-generated method stub
+		
+	}
+
+	@Override
+	public void update() {
+		// TODO Auto-generated method stub
+		
+	}
+
+	@Override
+	public void draw(Graphics2D g) {
+		for(int i = 0; i<pausa.length; i++){			
+			g.drawString(pausa[i], 320, 200+i*50);
+		}
+		
+	}
+
+	@Override
+	public void keyPressed(int k) {
+		if(k== KeyEvent.VK_P ){
+			this.cds.setState(ControllerDiSessione.LIVELLO1);
+		}
+		
+	}
+
+	@Override
+	public void keyReleased(int k) {
+		// TODO Auto-generated method stub
+		
+	}
+
+	@Override
+	public void mouseClicked() {
+		// TODO Auto-generated method stub
+		
+	}
+
+	@Override
+	public void mouseReleased() {
+		// TODO Auto-generated method stub
+		
+	}
+
+}
