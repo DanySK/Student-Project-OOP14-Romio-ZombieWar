40a2119e041664c55e654cd607abe8ca014f90c7
giovanniromio <giovanniromio@localhost>
2015-01-28 17:42:02 +0100

    Aggiunto EnrageZombie. Zombie che aumenta la sua velocit? quando in fin di vita. Modificati commenti. Modificati nomi classi.

---
+++ src/entities/ZombieMom.java
@@ -0,0 +1,22 @@
+package entities;
+
+public class ZombieMom extends Zombie {
+
+	public ZombieMom(int xSpawn, int ySpawn) {
+		super(xSpawn, ySpawn);
+	}
+	public void init(){
+		/* Load zombie image */
+		this.sprite = setSprite("/sprites/zombieMom.png");
+		/*The png file contain 5 different sprites wich make the walk animation*/
+		this.width = sprite.getWidth()/4;
+		this.height = sprite.getHeight();
+		/* Create the animation */
+		this.setCamminata(sprite, width, height);
+		this.hp = 25;	
+		this.damage = 1;
+		this.speed = 1;
+	}
+	
+
+}
